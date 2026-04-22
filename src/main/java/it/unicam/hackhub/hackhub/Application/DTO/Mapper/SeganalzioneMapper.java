594f11322cad7fd5f549dafaab318d43b504ab8d
Giulia <giulia02.marconi@studenti.unicam.it>
2026-04-22 18:43:11 +0200

    aggiunta SegnalazioneMapper

---
+++ src/main/java/it/unicam/hackhub/hackhub/Application/DTO/Mapper/SeganalzioneMapper.java
@@ -0,0 +1,42 @@
+package it.unicam.hackhub.hackhub.Application.DTO.Mapper;
+
+import it.unicam.hackhub.hackhub.Application.DTO.Request.SegnalazioneRequest;
+import it.unicam.hackhub.hackhub.Application.DTO.Response.SegnalazioneResponse;
+import it.unicam.hackhub.hackhub.Core.models.Hackathon;
+import it.unicam.hackhub.hackhub.Core.models.MembroStaff;
+import it.unicam.hackhub.hackhub.Core.models.Segnalazione;
+import it.unicam.hackhub.hackhub.Core.models.Team;
+
+public class SeganalzioneMapper {
+    public Segnalazione toEntity(SegnalazioneRequest request) {
+        if (request == null) return null;
+        Segnalazione segnalazione = new Segnalazione();
+        segnalazione.setTitolo(request.getTitolo());
+        segnalazione.setDescrizione(request.getDescrizione());
+
+        Team team = new Team();
+        team.setId(request.getIdTeam());
+        segnalazione.setTeamSegnalato(team);
+
+        Hackathon hackathon = new Hackathon();
+        hackathon.setId(request.getIdHackathon());
+        segnalazione.setHackathon(hackathon);
+
+        MembroStaff mentore = new MembroStaff();
+        mentore.setId(request.getIdMentore());
+        segnalazione.setMentore(mentore);
+
+        return segnalazione;
+    }
+
+    public SegnalazioneResponse toResponse(Segnalazione segnalazione) {
+        if (segnalazione == null) return null;
+        SegnalazioneResponse response = new SegnalazioneResponse();
+        response.setTitolo(segnalazione.getTitolo());
+        response.setDescrizione(segnalazione.getDescrizione());
+        response.setNomeTeamSegnalato(segnalazione.getTeamSegnalato().getNome());
+        response.setNomeHackathon(segnalazione.getHackathon().getNome());
+        response.setUsernameMentore(segnalazione.getMentore().getUsername());
+        return response;
+    }
+}
